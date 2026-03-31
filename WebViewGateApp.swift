97ab09d63c0e70f4c3bd6132889be98d32b67757
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-03-31 18:52:18 +0300

    Update WebViewGateApp.swift

--- WebViewGateApp.swift
+++ WebViewGateApp.swift
@@ -4,7 +4,7 @@ import UIKit
 @main
 struct YourAppNameApp: App {
     @StateObject private var appStore = AppStore()
-    @StateObject private var webViewGateService = WebUGateService()
+    @StateObject private var webViewGateService = WebViewGateService()
 
     @State private var isLaunchComplete = false
     @State private var showWebViewGate = false
